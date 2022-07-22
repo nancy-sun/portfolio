e2813c058d2299f67bb6ed48e7e09a6733e27536
Nancy Sun <99620863+nancy-sun@users.noreply.github.com>
2022-07-21 23:09:11 -0700

    trying out three.js

--- src/components/Box/Box.jsx
+++ src/components/Box/Box.jsx
@@ -1,15 +1,54 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Canvas, useFrame } from '@react-three/fiber'
-import { RoundedBox, meshPhongMaterial, GradientTexture } from "@react-three/drei";
-
+import { Canvas, useFrame, useLoader } from '@react-three/fiber'
+import { Text, PerspectiveCamera, RenderTexture, RoundedBox, Sphere } from "@react-three/drei";
+import * as THREE from 'three';
+// import one from "../../assets/textures/one.jpg"
+// import two from "../../assets/textures/two.jpg"
+// import three from "../../assets/textures/three.jpg"
 
 
 function Box() {
+
+    // const texture_1 = useLoader(THREE.TextureLoader, one)
+    // const texture_2 = useLoader(THREE.TextureLoader, two);
+    // const texture_3 = useLoader(THREE.TextureLoader, three);
+
+
+
+    const toSkills = (e) => {
+        window.location.replace("/skills");
+    }
+
+    // const textRef = useRef();
+    // useFrame((state) => (textRef.current.position.x = Math.sin(state.clock.elapsedTime) * 2))
+
+
+
     return (
-        <mesh>
-            <RoundedBox args={[1, 1, 1]} radius={0.05} smoothness={4}>
-                <meshPhongMaterial color="#a2d2ff" />
-            </RoundedBox>
+        <mesh rotation-y={0} onClick={toSkills}>
+            {/* <boxGeometry args={[1, 1, 1]} attach="geometry" /> */}
+            <Sphere>
+                <meshStandardMaterial attach="material" color="#a2d2ff" side={THREE.DoubleSide}>
+                    <RenderTexture attach="map" anisotropy={16}>
+                        <PerspectiveCamera makeDefault manual aspect={1 / 1} position={[0, 0, 5]} />
+                        <color attach="background" args={["#a2d2ff"]} />
+                        <ambientLight intensity={0.5} />
+                        <Text fontSize={1} color="#555">
+                            skills
+                        </Text>
+                    </RenderTexture>
+                </meshStandardMaterial>
+            </Sphere>
+            {/* <RoundedBox>
+                <RenderTexture attach="map" anisotropy={16}>
+                    <PerspectiveCamera makeDefault manual aspect={1 / 1} position={[0, 0, 5]} />
+                    <color attach="background" args={["#a2d2ff"]} />
+                    <ambientLight intensity={0.5} />
+                    <Text fontSize={1} color="#555">
+                        projects
+                    </Text>
+                </RenderTexture>
+            </RoundedBox> */}
         </mesh>
     )
 }
--- src/components/BoxCanvas/BoxCanvas.jsx
+++ src/components/BoxCanvas/BoxCanvas.jsx
@@ -2,15 +2,17 @@ import React, { Suspense } from 'react';
 import Box from '../Box/Box';
 import { Canvas } from "@react-three/fiber";
 import "./BoxCanvas.scss";
-import { OrbitControls, RoundedBox, meshPhongMaterial } from "@react-three/drei";
+import { OrbitControls, ContactShadows } from "@react-three/drei";
 
 function BoxCanvas() {
 
     return (
         <Canvas className="canvas">
-            <OrbitControls />
             <ambientLight intensity={0.6} />
-            <directionalLight position={[12, 15, 5]} />
+            <directionalLight position={[12, 15, 3]} />
+            <OrbitControls autoRotateSpeed={3} autoRotate />
+            {/* <ContactShadows frames={1} position={[0, -0.5, 0]} blur={1} opacity={0.75} />
+            <ContactShadows frames={1} position={[0, -0.5, 0]} blur={3} color="black" /> */}
             <Box />
 
         </Canvas>
