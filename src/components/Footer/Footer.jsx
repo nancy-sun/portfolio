0a6917dd72114c8c27fc5be091752d7895d95f02
Nancy Sun <99620863+nancy-sun@users.noreply.github.com>
2022-07-24 10:43:43 -0700

    change footer color based on theme

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./Footer.scss";
 
 function Footer() {
+    const theme = useSelector((state) => state.theme);
+
     return (
         <footer className="footer">
-            <a className="footer__text" href="https://github.com/nancy-sun/portfolio">{"© 2022 designed & developed by Nancy Sun"}</a>
-            <a className="footer__logo footer__logo--github" href="https://github.com/nancy-sun"></a>
-            <a className="footer__logo footer__logo--linkedin" href="https://www.linkedin.com/in/-nancy-sun/"></a>
+            <a target="_blank" className="footer__text" href="https://github.com/nancy-sun/portfolio">{"© 2022 designed & developed by Nancy Sun"}</a>
+            {/* <a className="footer__logo footer__logo--github" href="https://github.com/nancy-sun"></a> */}
+            <a target="_blank" className={theme.darkTheme ? "footer__logo footer__logo--github footer__logo--dark" : "footer__logo footer__logo--github"} href="https://github.com/nancy-sun"></a>
+            <a target="_blank" className={theme.darkTheme ? "footer__logo footer__logo--linkedin footer__logo--dark" : "footer__logo footer__logo--linkedin"} href="https://www.linkedin.com/in/-nancy-sun/"></a>
         </footer>
     )
 }
