fbdd1c9a7b2273a16677f7e2ba597b1bd11ff98e
Nancy Sun <99620863+nancy-sun@users.noreply.github.com>
2022-07-24 19:58:05 -0700

    issue to fix: navlink active classname

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -8,19 +8,25 @@ import "./Nav.scss";
 
 function Nav() {
 
-
     const theme = useSelector((state) => state.theme);
     const dispatch = useDispatch();
-
 
     return (
         <nav className="nav">
             <NavLink to="/" className="nav__logo">N</NavLink>
             <div className="nav__list">
-                <NavLink to="/about" className="nav__item">About</NavLink>
-                <NavLink to="/projects" className="nav__item">Projects</NavLink>
-                <NavLink to="/skills" className="nav__item">Skills</NavLink>
-                <NavLink to="/contact" className="nav__item">Contact</NavLink>
+                <NavLink to="/" className="nav__item--active">Home</NavLink>
+                <NavLink to="/about" className={isActive =>
+                    "nav__item" + (isActive ? " nav__item--active" : null)
+                }
+                    style={({ isActive }) => isActive ? { color: "green" } : {}}
+                >About</NavLink>
+                <NavLink to="/projects" className={isActive =>
+                    "nav__item" + (isActive ? " nav__item--active" : null)}>Projects</NavLink>
+                <NavLink to="/skills" className={isActive =>
+                    "nav__item" + (isActive ? " nav__item--active" : null)}>Skills</NavLink>
+                <NavLink to="/contact" className={isActive =>
+                    "nav__item" + (isActive ? " nav__item--active" : null)}>Contact</NavLink>
                 <FormGroup>
                     <FormControlLabel control={<Switch checked={theme.darkTheme} onChange={() => dispatch(toggleTheme())} />} />
                 </FormGroup>
