d3639f465ad634855658836e8772d5fa267b5e6b
Nancy Sun <99620863+nancy-sun@users.noreply.github.com>
2022-07-25 18:17:37 -0700

    fix navlink active styling

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -15,18 +15,11 @@ function Nav() {
         <nav className="nav">
             <NavLink to="/" className="nav__logo">N</NavLink>
             <div className="nav__list">
-                <NavLink to="/" className="nav__item--active">Home</NavLink>
-                <NavLink to="/about" className={isActive =>
-                    "nav__item" + (isActive ? " nav__item--active" : null)
-                }
-                    style={({ isActive }) => isActive ? { color: "green" } : {}}
-                >About</NavLink>
-                <NavLink to="/projects" className={isActive =>
-                    "nav__item" + (isActive ? " nav__item--active" : null)}>Projects</NavLink>
-                <NavLink to="/skills" className={isActive =>
-                    "nav__item" + (isActive ? " nav__item--active" : null)}>Skills</NavLink>
-                <NavLink to="/contact" className={isActive =>
-                    "nav__item" + (isActive ? " nav__item--active" : null)}>Contact</NavLink>
+                <NavLink to="/" className="nav__item" style={({ isActive }) => isActive ? { color: "green" } : {}}>Home</NavLink>
+                <NavLink to="/about" className="nav__item" style={({ isActive }) => isActive ? { color: "green" } : {}}>About</NavLink>
+                <NavLink to="/projects" className="nav__item" style={({ isActive }) => isActive ? { color: "green" } : {}}>Projects</NavLink>
+                <NavLink to="/skills" className="nav__item" style={({ isActive }) => isActive ? { color: "green" } : {}}>Skills</NavLink>
+                <NavLink to="/contact" className="nav__item" style={({ isActive }) => isActive ? { color: "green" } : {}}>Contact</NavLink>
                 <FormGroup>
                     <FormControlLabel control={<Switch checked={theme.darkTheme} onChange={() => dispatch(toggleTheme())} />} />
                 </FormGroup>
