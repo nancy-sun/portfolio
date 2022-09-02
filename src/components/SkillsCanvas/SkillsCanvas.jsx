32d5e466699c08f523260350629fcc64c8a1c809
Nancy Sun <99620863+nancy-sun@users.noreply.github.com>
2022-09-02 12:55:30 -0700

    update portfolio page

--- src/components/SkillsCanvas/SkillsCanvas.jsx
+++ src/components/SkillsCanvas/SkillsCanvas.jsx
@@ -8,12 +8,15 @@ import SkillsIcosahedron from "../SkillsIcosahedron/SkillsIcosahedron";
 import SkillsCube from '../SkillsCube/SkillsCube';
 import SkillsPlane from '../SkillsPlane/SkillsPlane';
 import jsIcon from "../../assets/icons/javascript.png";
+import tsIcon from "../../assets/icons/typescript.png";
 import sassIcon from "../../assets/icons/sass.png";
 import mysqlIcon from "../../assets/icons/mysql.png";
 import reactIcon from "../../assets/icons/react.png";
 import pythonIcon from "../../assets/icons/python.png";
 import nodeIcon from "../../assets/icons/nodejs.png";
 import mongoDBIcon from "../../assets/icons/mongodb.png";
+import javaIcon from "../../assets/icons/java.png";
+import graphQLIcon from "../../assets/icons/graphql.png";
 import "./SkillsCanvas.scss";
 import Background from "../Background/Background";
 
@@ -23,21 +26,24 @@ function SkillsCanvas() {
     return (
         <Canvas className="canvas">
             <OrbitControls
-                maxDistance={10} minDistance={1.4}
-                minPolarAngle={0} maxPolarAngle={Math.PI / 2.5}
+                maxDistance={10} minDistance={2.9}
+                minPolarAngle={-1.1} maxPolarAngle={Math.PI / 2.5}
                 enablePan={false}
             />
             <ambientLight intensity={theme ? 0.15 : 0.6} />
             <directionalLight position={[50, 30, 0]} />
             <Physics>
                 <SkillsPlane />
-                <SkillsCube scale={0.55} position={[0.7, 2, 1.3]} texture={mongoDBIcon} />
-                <SkillsCube scale={0.5} position={[-0.1, 4, 0]} texture={nodeIcon} />
-                <SkillsCube scale={0.5} position={[-0.1, 6.5, -1]} texture={pythonIcon} />
-                <SkillsCube scale={0.5} position={[-0.3, 3, 1]} texture={jsIcon} />
-                <SkillsSphere scale={0.4} position={[-0.3, 7.5, 2.4]} texture={reactIcon} />
-                <SkillsSphere scale={0.4} position={[1, 5, 0]} texture={mysqlIcon} />
-                <SkillsIcosahedron scale={0.45} position={[-1, 5, -1]} texture={sassIcon} />
+                <SkillsCube scale={0.5} position={[0.7, 2, 1.3]} texture={mongoDBIcon} />
+                <SkillsCube scale={0.48} position={[-0.1, 4, 0]} texture={nodeIcon} />
+                <SkillsCube scale={0.48} position={[-0.1, 6.5, -1]} texture={pythonIcon} />
+                <SkillsCube scale={0.48} position={[-0.3, 3, 1]} texture={jsIcon} />
+                <SkillsCube scale={0.48} position={[1, 2, 0.3]} texture={tsIcon} />
+                <SkillsSphere scale={0.3} position={[-0.3, 7.5, 2.4]} texture={reactIcon} />
+                <SkillsSphere scale={0.3} position={[-1, 2.2, -1.1]} texture={graphQLIcon} />
+                <SkillsSphere scale={0.34} position={[1, 5, 0]} texture={mysqlIcon} />
+                <SkillsCube scale={0.4} position={[0.7, 3, -1.1]} texture={javaIcon} />
+                <SkillsIcosahedron scale={0.34} position={[-1, 5, -1]} texture={sassIcon} />
             </Physics>
             <Background theme={theme} />
         </Canvas>
