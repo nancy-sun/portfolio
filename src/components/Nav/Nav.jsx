2a089d8ba2829497ad0e4d9e83d1fdfd4778483d
Nancy Sun <99620863+nancy-sun@users.noreply.github.com>
2022-11-07 12:59:50 -0800

    refactor nav styles and code

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { Switch, FormGroup, FormControlLabel } from "@mui/material";
+import { Switch, FormGroup, FormControlLabel, useMediaQuery } from "@mui/material";
 import { toggleTheme } from "../../reducers/themeSlice";
 import { useSelector, useDispatch } from "react-redux";
 import "./Nav.scss";
@@ -8,19 +8,29 @@ import "./Nav.scss";
 function Nav() {
     const theme = useSelector((state) => state.darkTheme);
     const dispatch = useDispatch();
+    const isLgScreen = useMediaQuery("(min-width:768px)");
+
+    console.log(isLgScreen)
 
     return (
         <div className="nav-wrap">
             <nav className="nav">
                 <NavLink to="/" className="nav__logo">N</NavLink>
-                <div className="nav__list">
-                    <NavLink to="/" className="nav__item" style={({ isActive }) => isActive ? { color: "#eb9aa8", fontWeight: 800 } : {}}>Home</NavLink>
-                    <NavLink to="/about" className="nav__item" style={({ isActive }) => isActive ? { color: "#eb9aa8", fontWeight: 800 } : {}}>About</NavLink>
-                    <NavLink to="/projects" className="nav__item" style={({ isActive }) => isActive ? { color: "#eb9aa8", fontWeight: 800 } : {}}>Projects</NavLink>
-                    <NavLink to="/skills" className="nav__item" style={({ isActive }) => isActive ? { color: "#eb9aa8", fontWeight: 800 } : {}}>Skills</NavLink>
-                    <NavLink to="/contact" className="nav__item" style={({ isActive }) => isActive ? { color: "#eb9aa8", fontWeight: 800 } : {}}>Contact</NavLink>
+                <div className="nav__list-wrap">
+                    <div className="nav__list">
+                        <NavLink to="/" className={({ isActive }) =>
+                            isActive ? "nav__item--active" : "nav__item"}>Home</NavLink>
+                        <NavLink to="/about" className={({ isActive }) =>
+                            isActive ? "nav__item--active" : "nav__item"}>About</NavLink>
+                        <NavLink to="/projects" className={({ isActive }) =>
+                            isActive ? "nav__item--active" : "nav__item"}>Projects</NavLink>
+                        <NavLink to="/skills" className={({ isActive }) =>
+                            isActive ? "nav__item--active" : "nav__item"}>Skills</NavLink>
+                        <NavLink to="/contact" className={({ isActive }) =>
+                            isActive ? "nav__item--active" : "nav__item"}>Contact</NavLink>
+                    </div>
                     <FormGroup className="nav__control">
-                        <FormControlLabel control={<Switch checked={theme} onChange={() => dispatch(toggleTheme())} />} />
+                        <FormControlLabel control={<Switch size={isLgScreen ? "medium" : "small"} checked={theme} onChange={() => dispatch(toggleTheme())} />} />
                     </FormGroup>
                 </div>
             </nav>
